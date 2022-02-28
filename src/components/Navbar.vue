ce15af71c87902bd2041e4fd701339d6c831e530
Oswaldo Adrián Osuna <oswaldo.adrian@live.com.mx>
2022-02-27 19:59:36 -0600

    Delete unused files

---
+++ src/components/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+  <!-- Navbar -->
+  <nav class="navbar navbar-primary">
+    <div class="container-fluid">
+      <h3>
+        <span class="navbar-brand text-white">
+          Pokedex
+        </span>
+      </h3>
+    </div>
+  </nav> <!-- END Navbar -->
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'NavbarComponent'
+});
+</script>
---
+++ src/layouts/PokedexLayout.vue
@@ -0,0 +1,18 @@
+<template>
+  <Navbar />
+  <h1>Pokedex Layout</h1>
+</template>
+
+<script lang="ts">
+
+import { defineComponent } from 'vue';
+import Navbar from '@/components/Navbar.vue';
+
+export default defineComponent({
+  name: 'PokedexLayout',
+  components: {
+    Navbar
+  }
+});
+
+</script>
